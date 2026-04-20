397cc6b09b4ab71affdbb741a6735671220a0bbf
omar-hossam <omarhossam50@proton.me>
2026-04-20 11:56:05 +0200

    refactor: delete complicated and make the project simple but impactful

--- app/routes/main.py
+++ app/routes/main.py
@@ -12,10 +12,22 @@ def index():
 def about():
     return "<h1>About our Donation Platform</h1>"
 
+
 @main_bp.route("/register")
 def register():
     return render_template("register.html")
 
-@main_bp.route("/donor-dashboard")
+
+@main_bp.route("/d-donor")
 def donor_dashboard():
-    return render_template("donor_dashboard.html")
+    return render_template("dashboards/donor.html")
+    
+    
+@main_bp.route("/d-hospital")
+def hospital_dashboard():
+    return render_template("dashboards/hospital.html")
+    
+    
+@main_bp.route("/d-admin")
+def admin_dashboard():
+    return render_template("dashboards/admin.html")
